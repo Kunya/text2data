d9c1a5d93234e2046820fa1cd114c59b893b60c8
kunya <alexander.kushch@gmail.com>
2019-05-07 14:57:38 +0300

    updated circleci config

--- server/service-s3local.js
+++ server/service-s3local.js
@@ -1,13 +1,11 @@
 var config = require('./config.json');
-var yazl = require("yazl");
 var S3FS = require('s3fs');
 var fs = new S3FS(config.s3.bucket, config.s3.options);
 var fslocal = require("fs");
 var path = require("path");
 
 const Queue = require('bee-queue');
-//const s3Local = new Queue('s3local', { removeOnSuccess: true });
-
+const s3local = new Queue('s3local', { redis: config.redis, removeOnSuccess: true });
 
 // TESTING
 
@@ -16,25 +14,24 @@ function test() {
     var data = { s3local: {} };
     
     
-    data.s3local.from = 'ipsos-data/_1000900830.jpg';
-    data.s3local.to='home/app/_1000900830.jpg';
+    data.s3local.from = '_1000900830.jpg';
+    data.s3local.to='_1000900830.jpg';
     data.s3local.action='download';
     
     var job={data:data};
-    brozzy(job,(x,y)=>{consolge.log(x + ". Data=" + y)}));
+    //brozzy(job,(x,y)=>{console.log(x + ". Data=" + y)});
      
-    /*const thatQueue = new Queue("s3local", { removeOnSuccess: true });
+    const thatQueue = new Queue("s3local", { redis:config.redis, removeOnSuccess: true });
 
     const job = thatQueue.createJob(data);
     job.on('failed', (err) => { console.log(err); });
     job.on('succeeded', (result) => { console.log(result); });
-    job.save().then(() => { console.log("Job was added."); }).catch(function(err) { console.log(err); });
-    */
+    job.save().then(() => { console.log("S3local job added."); }).catch(function(err) { console.log(err); });
+    
 }
 
 
-//s3Local.process(function
-function brozzy(job, done) {
+s3local.process(function(job, done) {
     var data=job.data.s3local;
     if (!data) return done(new Error('S3-local service: no data provided.'));
 
@@ -47,13 +44,12 @@ function brozzy(job, done) {
         case 'download':
           var strm=fs.createReadStream(data.from);
           strm.on("end",()=>{done(null)});
-          strm.pipe(fslocal.createWriteStream(data.to);
+          strm.pipe(fslocal.createWriteStream(data.to));
           break;
         default: return done(new Error('S3-local service: missing or unknown "action" parameter. Expect "download" or "upload".'));
       }
     
-});
+}); 
 
-
-test();
+//test();
 
